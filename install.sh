44a99c34f2de7e606976f54fb03db13936f2c947
Bryan Hann <nobody@nowhere>
2019-07-10 23:41:06 +0800

    Add configuration installer

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 [ "$0" == './install.sh' ] || echo try "./install.sh"
 [ "$0" == './install.sh' ] || exit
-mkdir ~/.config
-mkdir ~/.config/rhino
-echo export RHINO=$PWD >> ~/.config/rhino/source
-echo [$RHINO]
+mkdir -p ~/.config/rhino
+echo export RHINO=$PWD > ~/.config/rhino/source
+echo source \$RHINO/config/source  >> ~/.config/rhino/source
 cat ~/.config/rhino/source
-source ~/.config/rhino/source
-echo [$RHINO]
